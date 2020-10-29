0c6270f8a2bdb959406138cc8aeff282534c125d
jimin755 <57628980+jimin755@users.noreply.github.com>
2020-10-30 01:30:11 +0900

    Update Map.java

--- app/src/main/java/com/example/project/Map.java
+++ app/src/main/java/com/example/project/Map.java
@@ -1,9 +1,9 @@
 package com.example.project;
 
-import java.util.*;
+        import java.util.*;
 
 /**
- * 
+ *
  */
 public abstract class Map {
 
@@ -14,36 +14,20 @@ public abstract class Map {
     }
 
     /**
-     * 
+     *
      */
-    public ArrayList<Place> near_places;
-    public Place center_place;
-    public UserLoc user_place;
+    public ArrayList<Place> nearPlaces;
+    public Place centerPlace;
+    public UserLoc userPlace;
 
 
 
     /**
-     * 
+     *
      */
     public abstract void print_marker();
-
-    /**
-     * 
-     */
     public abstract void input_marker();
-
-    /**
-     * @param arraylist<Patient> 
-     * @return
-     */
     public abstract ArrayList<Place> search_nearPlaces(ArrayList<Patient> patientsList);
-
-    /**
-     * @param Integer x1
-     * @param int y1 
-     * @param int x2 
-     * @param int y2
-     */
     public void print_map(Integer x1, Integer y1, Integer x2, Integer y2) {
         // TODO implement here
     }
