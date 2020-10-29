e0b80f04b950fe8185aa0eb5319be03ebacafcfe
jimin755 <57628980+jimin755@users.noreply.github.com>
2020-10-29 23:31:20 +0900

    Update UserLoc.java

--- app/src/main/java/com/example/project/UserLoc.java
+++ app/src/main/java/com/example/project/UserLoc.java
@@ -20,12 +20,12 @@ public class UserLoc extends AppCompatActivity {
      * Default constructor
      */
     private final int MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 200;
-
     private Place user_place;
-    private Integer mode;
 
     // 1: 사용자 위치정보 허용 상태, 2: 사용자 위치 정보 허용하지 않은 상태
     public UserLoc() {
+        this.Pemission();
+        this.Locby_gps();
     }
 
     public Place getPlace() {
@@ -33,15 +33,11 @@ public class UserLoc extends AppCompatActivity {
     }
 
     public Integer getMode() {
-        return mode;
+        return ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
     }
 
     public void setUser_place(Place p) {
         this.user_place = p;
-    }
-
-    public void setMode(Integer m) {
-        this.mode = m;
     }
 
     public void Loc_by_Search() {
@@ -50,19 +46,14 @@ public class UserLoc extends AppCompatActivity {
 
     //권한 확인후 권한 요청
     public void Pemission() {
-        int mode = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION); // 권한 확인
-        if (mode == PERMISSION_DENIED) {
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
-                    Manifest.permission.ACCESS_FINE_LOCATION)) {
-                Toast.makeText(this, "사용자 위치정보 동의 거부시 사용이 제한되는 부분이 있을수 있습니다.", Toast.LENGTH_LONG).show();
-            }
-        } else {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PERMISSION_DENIED) {
+            Toast.makeText(this, "사용자 위치정보 동의 거부시 사용이 제한되는 부분이 있을수 있습니다.", Toast.LENGTH_LONG).show();
             ActivityCompat.requestPermissions(this,
                     new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
                     MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
-            Toast.makeText(this, "사용자 위치정보 동의 거부시 사용이 제한되는 부분이 있을수 있습니다.", Toast.LENGTH_LONG).show();
-        }
+        } // 권한확인후 위치정보 제공 동의가 안 되어 있을때 위치 정보 제공 동의받기
     }
+
     public void Locby_gps() {
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
                     ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
@@ -72,12 +63,12 @@ public class UserLoc extends AppCompatActivity {
             this.user_place.set_x(location.getLatitude());//경도
             this.user_place.set_y(location.getLongitude());//위도
             this.user_place.get_address(location.getProvider());//해당 위치정보
-        }// 위치정보 제공 동의가 되어있을때
+        }// 위치정보 제공 동의가 되어있을때 정보를 받아옴
         else{
-            this.user_place.set_x(37.55746);//위도
-            this.user_place.set_y(127.00059);//경도
-            this.user_place.get_address("동국대학교");//해당 위치정보
-        }//제공 동의가 안되어 있때 유저위치를 동국대학교로 디폴트 값 생성
+            this.user_place.set_x(37.558560);//위도
+            this.user_place.set_y(126.998935);//경도
+            this.user_place.get_address("동국대학교 원흥관");//해당 위치정보
+        }//제공 동의가 안되어 있때 유저위치를 동국대학교 원흥관으로 디폴트 값 생성
     }
 
 }
