b7eaf93d3a29add487a5f99cbaae330025d678db
jimin755 <57628980+jimin755@users.noreply.github.com>
2020-10-31 03:09:54 +0900

    Update LocalStatistics.java

--- app/src/main/java/com/example/project/LocalStatistics.java
+++ app/src/main/java/com/example/project/LocalStatistics.java
@@ -9,29 +9,44 @@ public class LocalStatistics extends Statistics {
     /**
      * Default constructor
      */
-    public LocalStatistics() {
+    public LocalStatistics(Date staticsDate,Integer patientNum,Integer deadNum,Integer healerNum,String localName,Place localPosition, Integer increaseDecrease) {
+    this.staticsDate=staticsDate;
+    this.patientNum=patientNum;
+    this.deadNum =deadNum;
+    this.healerNum =healerNum;
+    this.localName =localName;
+    this.localPosition =localPosition;
+    this.increaseDecrease=increaseDecrease;
+    }
+    private String localName; /*지역 이름*/
+    private Place localPosition; /*지역 시청 or 군청 좌표*/
+    private Integer increaseDecrease; /*전일 대비 증감 수*/
+
+    public Place getLocalPosition() {
+        return this.localPosition;
+    }
+    public String getLocalName() {
+        return this.localName;
+    }
+    public Date getStaticsDate(){
+        return this.staticsDate;
+    }
+    public Integer getPatientNum() {
+        return this.patientNum;
+    }
+    public Integer getDeadNum(){
+        return this.deadNum;
+    }
+    public Integer getHealerNum() {
+        return this.healerNum;
+    }
+    public Integer getIncreaseDecrease() {
+        return increaseDecrease;
     }
 
-    /**
-     * 
-     */
-    private String local_name;
-
-    /**
-     * 
-     */
-    private Place local_position;
-
-
-    /**
-     * 
-     */
     public void make_chart() {
         // TODO implement here
     }
 
-    /**
-     * 
-     */
 
 }
