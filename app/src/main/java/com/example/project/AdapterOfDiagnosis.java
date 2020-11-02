5605542cb85489d4b744fb9b5ae716927a88d1be
jun991203 <jun991203@naver.com>
2020-11-03 01:25:29 +0900

    자가진단 페이지 기본틀 업데이트

--- app/src/main/java/com/example/project/AdapterOfDiagnosis.java
+++ app/src/main/java/com/example/project/AdapterOfDiagnosis.java
@@ -1,6 +1,8 @@
 package com.example.project;
 
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,7 +16,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 
-public class AdapterOfDiagnosis extends RecyclerView.Adapter<AdapterOfDiagnosis.DiagnosisViewHolder> {
+public class AdapterOfDiagnosis extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
     private static final int TYPE_ITEM = 1;
     private static final int TYPE_FOOTER = 2;
@@ -32,39 +34,84 @@ public class AdapterOfDiagnosis extends RecyclerView.Adapter<AdapterOfDiagnosis.
         }
     }
 
+    public static class SubmitViewHolder extends RecyclerView.ViewHolder {
+        public Button SubmitButton;
+        public SubmitViewHolder(View v){
+            super(v);
+            SubmitButton = v.findViewById(R.id.selfdiagnosis_submit_Button);
+        }
+    }
+
     public AdapterOfDiagnosis(ArrayList<String> data){
         this.questionList = data;
     }
 
-    public DiagnosisViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.row_selfdiagnosis_question, parent, false);
-        return new DiagnosisViewHolder(view);
+    @Override
+    public int getItemViewType(int position){
+        if (position == questionList.size())
+            return TYPE_FOOTER;
+        else
+            return TYPE_ITEM;
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view;
+        RecyclerView.ViewHolder holder;
+        if (viewType == TYPE_FOOTER){
+            view = LayoutInflater.from(parent.getContext()).inflate(R.layout.row_selfdiagnosis_footer, parent, false);
+            holder = new SubmitViewHolder(view);
+        }else{
+            view = LayoutInflater.from(parent.getContext()).inflate(R.layout.row_selfdiagnosis_question, parent, false);
+            holder = new DiagnosisViewHolder(view);
+        }
+        return holder;
     }
 
     @Override
-    public void onBindViewHolder(@NonNull AdapterOfDiagnosis.DiagnosisViewHolder holder, final int position) {
-       holder.QuestionTextView.setText(questionList.get(position));
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, final int position) {
+        final ArrayList<String> yesList = new ArrayList<>();
+        /*question row*/
+        if (holder instanceof DiagnosisViewHolder){
+            final DiagnosisViewHolder Qholder = (DiagnosisViewHolder) holder;
+            Qholder.QuestionTextView.setText(questionList.get(position));
+            Qholder.YesButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    yesList.add(Integer.toString(position));
+                    Qholder.YesButton.setBackgroundColor(Color.parseColor("#E1E1E1"));
+                    Qholder.NoButton.setBackgroundColor(Color.parseColor("#FFFFFF"));
+                }
+            });
+            Qholder.NoButton.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View view) {
+                    if (yesList.contains(Integer.toString(position-1))){
+                        yesList.remove(Integer.toString(position-1));
+                        Qholder.YesButton.setBackgroundColor(Color.parseColor("#FFFFFF"));
+                        Qholder.NoButton.setBackgroundColor(Color.parseColor("#E1E1E1"));
+                    }
+                }
+            });
+        }
+        /*footer row*/
+        else if(holder instanceof SubmitViewHolder){
+            SubmitViewHolder Bholder = (SubmitViewHolder) holder;
+            Bholder.SubmitButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent(view.getContext(), PageOfSelfDiagnosisResult.class);
+                    intent.putExtra("result", yesList);
+                    view.getContext().startActivity(intent);
+                }
+            });
+        }
 
-       final ArrayList<String> yesList = new ArrayList<>();
-       holder.YesButton.setOnClickListener(new View.OnClickListener() {
-           @Override
-           public void onClick(View view) {
-               yesList.add(Integer.toString(position));
-           }
-       });
-
-       holder.NoButton.setOnClickListener(new View.OnClickListener(){
-           @Override
-           public void onClick(View view) {
-               if (yesList.contains(Integer.toString(position))){
-                   yesList.remove(Integer.toString(position));
-               }
-           }
-       });
     }
 
     @Override
     public int getItemCount() {
-        return (null != questionList ? questionList.size() : 0);
+        return (null != questionList ? questionList.size()+1 : 0);
     }
 }
---
+++ app/src/main/java/com/example/project/PageOfSelfDiagnosisResult.java
@@ -0,0 +1,14 @@
+package com.example.project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PageOfSelfDiagnosisResult extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_diagnosis_result);
+    }
+}
