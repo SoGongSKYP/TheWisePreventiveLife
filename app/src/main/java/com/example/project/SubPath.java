1de19238255080291506f183f3dca23c0e79d4a1
jimin755 <57628980+jimin755@users.noreply.github.com>
2020-11-06 17:26:11 +0900

    Update SubPath.java

--- app/src/main/java/com/example/project/SubPath.java
+++ app/src/main/java/com/example/project/SubPath.java
@@ -32,10 +32,10 @@ public class SubPath {
     private int trafficType; //이동 수단 종류 (도보, 버스, 지하철) 1-지하철, 2-버스, 3-도보
     private double distance; //이동 거리
     private int sectionTime; //이동 소요시간
-    private int stationCount; //이동 정거장 수(지하철, 버스 경우만 필수)
     private ArrayList<SubwayLane> subwayLanes; //1-9-3-5 교통 수단 정보 확장 노드 지하철 용
     private ArrayList<BusLane> busLanes; //1-9-3-5 교통 수단 정보 확장 노드 버스 용
 
+    private int stationCount; //이동 정거장 수(지하철, 버스 경우만 필수)
     private Place startStation; // 승차정류장 역명 , 역 x(경도)좌표, 역 Y(위도 좌표)
     private Place endStation; // 하차 정류장 역명 , 역 x(경도)좌표, 역 Y(위도 좌표)
     private String way; // 방면 정보
