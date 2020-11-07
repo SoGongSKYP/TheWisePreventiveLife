cdfb2a62f0bd2bf28135306fa0f833c08e5d2a68
psystar99 <soyoung99.park@gmail.com>
2020-11-07 12:44:25 +0900

    db commit4-PTM

--- app/src/main/java/com/example/project/DB.java
+++ app/src/main/java/com/example/project/DB.java
@@ -1,5 +1,6 @@
 package com.example.project;
 import android.os.AsyncTask;
+import android.util.Log;
 
 import java.net.ProtocolException;
 import java.sql.Connection;
@@ -14,48 +15,47 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 public class DB  extends AsyncTask<String, Void, String> {
-   // private ArrayList<Patient> patients;
-   // private  URL url;
-    //private HttpURLConnection conn;
+    /*원래 설했던 필드들*/
+     private ArrayList<Patient> patients;
+     public void connect_DB() { }
+     public ArrayList<Patient> bring_patient_infoNroute() { return null; }
+     public boolean update_DB(ArrayList<Patient> patientslist) { return true; }
+     public boolean check_user_idpw(String id, String pw) throws IOException { return true;}
+
+    //서버로 보낼 메세지, 받을 메세
     String sendMsg, receiveMsg;
+    // 접속할 서버 주소 (이클립스에서 android.jsp 실행시 웹브라우저 주소)
+    String serverip="http://192.168.1.196:8080/WLP/androidDB.jsp";
 
-
-    public void connect_DB() {
+    //클래스 생성자
+    DB(String sendMsg){
+        this.sendMsg=sendMsg;
     }
-
-    /**
-     * @return
-     */
-    public ArrayList<Patient> bring_patient_infoNroute() {
-        // TODO implement here
-        return null;
-    }
-    public boolean update_DB(ArrayList<Patient> patientslist) {
-        // TODO implement here
-        return true;
-    }
-    public boolean check_user_idpw(String id, String pw) throws IOException {
-
-return true;
-}
-
+    //
     @Override
     protected String doInBackground(String... strings) {
 
         try {
             String str;
-
-            // 접속할 서버 주소 (이클립스에서 android.jsp 실행시 웹브라우저 주소)
-            URL url = new URL("http://192.168.1.196:8080/WLP/androidDB.jsp");
-
+            URL url = new URL(serverip);
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
             conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
             conn.setRequestMethod("POST");
             OutputStreamWriter osw = new OutputStreamWriter(conn.getOutputStream());
 
-            // 전송할 데이터. GET 방식으로 작성
-            sendMsg = "id=" + strings[0] + "&pw=" + strings[1];
+            /**
+             *  전송할 데이터. POST 방식으로 작성---sendMsg에따라서 디비에 요청하는 메세지가 달라
+             * if(sendMsg.equals("vision_write")){
+             *                 sendMsg = "vision_write="+strings[0]+"&type="+strings[1];
+             *             }else if(sendMsg.equals("vision_list")){
+             *                 sendMsg = "&type="+strings[0];
+             *             }
+             **/
 
+            if(sendMsg.equals("test")){
+            sendMsg = "id=" + strings[0] + "&pw=" + strings[1]+"&type="+strings[2];}
+
+            //안드로이드에서 jsp 서버로 통신값 보내
             osw.write(sendMsg);
             osw.flush();
 
@@ -72,6 +72,7 @@ return true;
                 receiveMsg = buffer.toString();
             } else {
                 // 통신 실패
+                Log.i("통신 결과", conn.getResponseCode()+"에러");
             }
         } catch (MalformedURLException e) {
             e.printStackTrace();
