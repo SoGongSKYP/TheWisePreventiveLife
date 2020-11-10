49c34fd657241869f2e3bd85f2de591fbc65551f
jimin755 <57628980+jimin755@users.noreply.github.com>
2020-11-10 16:51:34 +0900

    Update SelectedClinic.java

--- app/src/main/java/com/example/project/SelectedClinic.java
+++ app/src/main/java/com/example/project/SelectedClinic.java
@@ -1,5 +1,20 @@
 package com.example.project;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
 import java.util.*;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 
 /**
  * 
@@ -58,10 +73,10 @@ public class SelectedClinic {
     }
 
     /*직선거리 구하기 위한 함수*/
-    public double Distance(double lat1, double lon1, double lat2, double lon2, String unit) {
+    public double Distance(double lat1, double lon1, String unit) {
 
-        double theta = lon1 - lon2;
-        double dist = Math.sin(deg2rad(lat1)) * Math.sin(deg2rad(lat2)) + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.cos(deg2rad(theta));
+        double theta = lon1 - place.get_placeY();
+        double dist = Math.sin(deg2rad(lat1)) * Math.sin(deg2rad(place.get_placeX())) + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(place.get_placeX())) * Math.cos(deg2rad(theta));
 
         dist = Math.acos(dist);
         dist = rad2deg(dist);
@@ -72,7 +87,6 @@ public class SelectedClinic {
         } else if(unit == "meter"){
             dist = dist * 1609.344;
         }
-
         return (dist);
     }
 
@@ -87,9 +101,64 @@ public class SelectedClinic {
         return (rad * 180 / Math.PI);
     }
 
-    private Place calXY(){
+    private static String getTagValue(String tag, Element eElement) {
+        NodeList nlList=eElement.getElementsByTagName(tag).item(0).getChildNodes();
+        Node nValue=(Node)nlList.item(0);
+        if(nValue==null) return null;
+        return nValue.getNodeValue();
+    }
 
-        return null;
+    public Place calXY(String name) throws IOException, ParserConfigurationException, SAXException {
+        String parsingUrl="";
+        Place searchLoc=null;
+
+        StringBuilder urlBuilder = new StringBuilder("https://maps.googleapis.com/maps/api/place/findplacefromtext/"); /*URL*/
+        urlBuilder.append("xml?" + URLEncoder.encode("input","UTF-8") + "="+URLEncoder.encode(name, "UTF-8")); /*장소 text*/
+        urlBuilder.append("&" + URLEncoder.encode("inputtype","UTF-8") + "="+ URLEncoder.encode("textquery", "UTF-8")); /*입력 형식 text로 설정*/
+        urlBuilder.append("&" + URLEncoder.encode("language","UTF-8") + "=" + URLEncoder.encode("ko", "UTF-8")); /*리턴 정보 한국어로 리턴*/
+        urlBuilder.append("&" + URLEncoder.encode("fields","UTF-8") + "=" + URLEncoder.encode("formatted_address,name,geometry", "UTF-8")); /*반환 받을 값들*/
+        urlBuilder.append("&" + URLEncoder.encode("key","UTF-8") + "=" + URLEncoder.encode("AIzaSyCjdZL_BjLqCcj0PBKGcUP6kteb5tV2syE", "UTF-8")); /*키 값*/
+        URL url = new URL(urlBuilder.toString());
+        parsingUrl=url.toString();
+        //System.out.println(parsingUrl);
+
+        DocumentBuilderFactory dbFactory=DocumentBuilderFactory.newInstance();
+        DocumentBuilder dBuilder=dbFactory.newDocumentBuilder();
+        Document doc=dBuilder.parse(parsingUrl);
+        doc.getDocumentElement().normalize();
+        //System.out.println("Root element : "+doc.getDocumentElement().getNodeName());
+
+        NodeList nList=doc.getElementsByTagName("candidates"); //장소 전체 노드
+
+        NodeList geoList=null;//doc.getElementsByTagName("geometry"); // 지역 노드
+        NodeList locList=null;//doc.getElementsByTagName("location"); // 지역 노드
+
+        NodeList openTimeList=null;//doc.getElementsByTagName("opening_hours"); // 지역 노드
+        //System.out.println("파싱할 리스트 수 : "+nList.getLength());
+
+        for(int i=0; i<nList.getLength(); i++) {
+            Node nNode=nList.item(i);
+            if(nNode.getNodeType()==Node.ELEMENT_NODE) {
+                Element eElement=(Element) nNode;
+                searchLoc.set_placeAddress(getTagValue("formatted_address",eElement));
+
+                geoList = eElement.getElementsByTagName("geometry");
+                for(int g =0; g<geoList.getLength();g++){
+                    Node geoNode=geoList.item(g);
+                    if(geoNode.getNodeType()==Node.ELEMENT_NODE){
+                        Element geoElement=(Element) geoNode;
+                        locList = geoElement.getElementsByTagName("location");
+                        Node locNode=locList.item(0);
+                        if(locNode.getNodeType()==Node.ELEMENT_NODE){
+                            Element locElement=(Element) locNode;
+                            searchLoc.set_placeX(Double.parseDouble(getTagValue("lat",locElement)));//경도
+                            searchLoc.set_placeY(Double.parseDouble(getTagValue("lng",locElement)));//위도
+                        }
+                    }
+                }
+            }
+        }
+        return searchLoc;
     }
 
 }
