d82a18c671de8403b6fbd81eca2d6702ca7efef5
jimin755 <57628980+jimin755@users.noreply.github.com>
2020-11-15 18:10:04 +0900

    Update PageOfIntro.java

--- app/src/main/java/com/example/project/PageOfIntro.java
+++ app/src/main/java/com/example/project/PageOfIntro.java
@@ -1,9 +1,16 @@
 package com.example.project;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+
+import static android.content.pm.PackageManager.PERMISSION_DENIED;
 
 public class PageOfIntro extends AppCompatActivity {
 
@@ -11,7 +18,7 @@ public class PageOfIntro extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
-
+        this.LocPermission(this,getApplicationContext());
         new android.os.Handler().postDelayed(
                 new Runnable() {
                     @Override
@@ -22,4 +29,13 @@ public class PageOfIntro extends AppCompatActivity {
                 }
         , 3000);
     }
+    private final int MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 200;
+    public void LocPermission(Activity activity , Context context) {
+        if (ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) == PERMISSION_DENIED ) {
+            //Toast.makeText(context, "사용자 위치정보 동의 거부시 사용이 제한되는 부분이 있을수 있습니다.", Toast.LENGTH_LONG).show();
+            ActivityCompat.requestPermissions(activity,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+        } // 권한확인후 위치정보 제공 동의가 안 되어 있을때 위치 정보 제공 동의받기
+    }
 }
+
