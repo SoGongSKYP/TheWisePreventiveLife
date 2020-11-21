f9d8910155d92fb14665859b50bb3c1c28c1ed0d
jimin755 <57628980+jimin755@users.noreply.github.com>
2020-11-22 01:53:18 +0900

    Update UserLoc.java

--- app/src/main/java/com/example/project/UserLoc.java
+++ app/src/main/java/com/example/project/UserLoc.java
@@ -35,6 +35,8 @@ public class UserLoc extends AppCompatActivity {
      */
     //private final int MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 200;
     private static Place userPlace = new Place("동국대학교 정보문화관", 37.559562, 126.998557);
+    private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 10;
+    private static final long MIN_TIME_BW_UPDATES = 1000 * 60 * 1;
 
     // 1: 사용자 위치정보 허용 상태, 2: 사용자 위치 정보 허용하지 않은 상태
     public UserLoc() {
@@ -50,6 +52,47 @@ public class UserLoc extends AppCompatActivity {
 
     public static void setUser_place(Place place) {
         userPlace = place;
+    }
+
+    public static void LocBy_gps(Context context, LocationListener gpsListener) {
+        //PageOfMain.GPSListener gpsListener = new PageOfMain.GPSListener();
+        try {
+            LocationManager locationManager = (LocationManager) context.getSystemService(LOCATION_SERVICE);
+            boolean isGPSEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+            boolean isNetworkEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+            Location location = null;
+            if (!isGPSEnabled && !isNetworkEnabled) {
+            } else {
+                int hasFineLocationPermission = ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION);
+                System.out.println("hasFineLocationPermission: " + Integer.toString(hasFineLocationPermission));
+                if (hasFineLocationPermission == PackageManager.PERMISSION_GRANTED) {
+                    if (isNetworkEnabled) {
+                        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, MIN_TIME_BW_UPDATES, MIN_DISTANCE_CHANGE_FOR_UPDATES, (LocationListener) gpsListener);
+                        if (locationManager != null) {
+                            location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+                            if (location != null) {
+                                UserLoc.getUserPlace().set_placeX(location.getLatitude());
+                                UserLoc.getUserPlace().set_placeY(location.getLongitude());//위
+                            }
+                        }
+                    }
+                    if (isGPSEnabled) {
+                        if (location == null) {
+                            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, MIN_TIME_BW_UPDATES, MIN_DISTANCE_CHANGE_FOR_UPDATES, (LocationListener) gpsListener);
+                            if (locationManager != null) {
+                                location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                                if (location != null) {
+                                    UserLoc.getUserPlace().set_placeX(location.getLatitude());
+                                    UserLoc.getUserPlace().set_placeY(location.getLongitude());//위도
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        } catch (Exception e) {
+            Log.d("@@@", "" + e.toString());
+        }
     }
 
     /*장소 검색*/
