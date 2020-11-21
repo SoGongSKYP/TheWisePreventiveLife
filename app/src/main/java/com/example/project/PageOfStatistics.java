1abad39a7d221950f05133b85a62a1921274fa9e
jimin755 <57628980+jimin755@users.noreply.github.com>
2020-11-21 18:05:23 +0900

    Update PageOfStatistics.java

--- app/src/main/java/com/example/project/PageOfStatistics.java
+++ app/src/main/java/com/example/project/PageOfStatistics.java
@@ -23,7 +23,8 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLEncoder;
 import java.text.ParseException;
-import java.util.*;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -40,7 +41,6 @@ public class PageOfStatistics extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
         View v = inflater.inflate(R.layout.fragment_user_statistics, container, false);
-
         btn_gyeonggido = v.findViewById(R.id.gyeonggi_Button);
         btn_gangwondo = v.findViewById(R.id.gangwondo_Button);
         btn_chungbuk = v.findViewById(R.id.chungBuk_Button);
@@ -70,8 +70,11 @@ public class PageOfStatistics extends Fragment {
 
     public PageOfStatistics() throws ParserConfigurationException, SAXException, ParseException, IOException {
         this.userPlace=new UserLoc();
+        final Lock lock = new ReentrantLock(); // lock instance
         this.nationStatistic = APIEntity.getNation();
-        this.findLoc();
+        geoThread geo = new geoThread(this,lock);
+        Thread thread = new Thread(geo);
+        thread.start();
         if(userPlace.getUserPlace().get_placeAddress() == "제주"){
             localNum=1;
         }
@@ -127,70 +130,6 @@ public class PageOfStatistics extends Fragment {
 
     private UserLoc userPlace;
     private NationStatistics nationStatistic;
-
-    private static String getTagValue(String tag, Element eElement) {
-        NodeList nlList=eElement.getElementsByTagName(tag).item(0).getChildNodes();
-        Node nValue=(Node)nlList.item(0);
-        if(nValue==null) return null;
-        return nValue.getNodeValue();
-    }
-
-    private void findLoc() throws IOException, ParserConfigurationException, SAXException, ParseException {
-        String parsingUrl="";
-        Place searchLoc=null;
-        String state=null;
-        String shortLocName=null;
-
-        StringBuilder urlBuilder = new StringBuilder("https://maps.googleapis.com/maps/api/geocode/xml"); /*URL*/
-        urlBuilder.append("?" + URLEncoder.encode("latlng","UTF-8") + "="
-                +URLEncoder.encode(Double.toString(this.userPlace.getUserPlace().get_placeY()), "UTF-8")+","
-                +URLEncoder.encode(Double.toString(this.userPlace.getUserPlace().get_placeX()), "UTF-8") ); /*여기는 위도 경도 y,x순*/
-        urlBuilder.append("&" + URLEncoder.encode("language","UTF-8") + "=" + URLEncoder.encode("ko", "UTF-8")); /*리턴 정보 한국어로 리턴*/
-        urlBuilder.append("&" + URLEncoder.encode("key","UTF-8") + "=" + URLEncoder.encode("AIzaSyCjdZL_BjLqCcj0PBKGcUP6kteb5tV2syE", "UTF-8")); /*키 값*/
-        URL url = new URL(urlBuilder.toString());
-        parsingUrl=url.toString();
-        //System.out.println(parsingUrl);
-
-        DocumentBuilderFactory dbFactory=DocumentBuilderFactory.newInstance();
-        DocumentBuilder dBuilder=dbFactory.newDocumentBuilder();
-        Document doc=dBuilder.parse(parsingUrl);
-
-        doc.getDocumentElement().normalize();
-        //System.out.println("Root element : "+doc.getDocumentElement().getNodeName());
-
-        NodeList geoList=doc.getElementsByTagName("GeocodeResponse"); //장소 전체 노드
-
-        NodeList status=null;//doc.getElementsByTagName("status"); // 지역 노드
-        NodeList resultList=null;//doc.getElementsByTagName("result"); // 지역 노드
-
-        NodeList ComponentList=null;//doc.getElementsByTagName("address_component"); // 지역 노드
-
-        for(int i=0; i<geoList.getLength(); i++) {
-            Node geoNode=geoList.item(i);
-            if(geoNode.getNodeType()==Node.ELEMENT_NODE) {
-                Element geoElement=(Element) geoNode;
-                state= getTagValue("status",geoElement); // 상태(오류)
-                resultList = geoElement.getElementsByTagName("result");
-                Node resultNode=resultList.item(i);
-                if(resultNode.getNodeType()==Node.ELEMENT_NODE){
-                    Element resultElement=(Element) resultNode;
-                    ComponentList =resultElement.getElementsByTagName("address_component");
-                    for(int c =0; c<ComponentList.getLength();c++){
-                        Node ComponentNode=ComponentList.item(c);
-                        if(ComponentNode.getNodeType()==Node.ELEMENT_NODE){
-                            Element ComponentElement=(Element) ComponentNode;
-                            if(getTagValue("type",ComponentElement)=="administrative_area_level_1"){
-                                shortLocName=getTagValue("short_name",ComponentElement);
-                                break;
-                            }
-                        }
-                    }
-                }
-            }
-        }
-        this.userPlace.getUserPlace().set_placeAddress(shortLocName); // 사용자가 있는 지역 이름 받음
-    } //역 지오 코딩
-
 
     public void nationStatistics() {
         System.out.println("전국");
@@ -329,5 +268,106 @@ public class PageOfStatistics extends Fragment {
         btn_ulsan.setOnClickListener(localClickListener);
         btn_sejong.setOnClickListener(localClickListener);
     }
+    public UserLoc getUserPlace() {
+        return userPlace;
+    }
+}
+class geoThread implements Runnable{
+    PageOfStatistics page;
+    private final Lock lock;
+    geoThread(PageOfStatistics page, Lock lock){
+        this.page=page;
+        this.lock = lock;
+    }
+    private static String getTagValue(String tag, Element eElement) {
+        NodeList nlList=eElement.getElementsByTagName(tag).item(0).getChildNodes();
+        Node nValue=(Node)nlList.item(0);
+        if(nValue==null) return null;
+        return nValue.getNodeValue();
+    }
+    @Override
+    public void run() {
+        String parsingUrl="";
+        Place searchLoc=null;
+        String state=null;
+        String shortLocName=null;
+        lock.lock();
+        try{
+            StringBuilder urlBuilder = new StringBuilder("https://maps.googleapis.com/maps/api/geocode/xml"); /*URL*/
+            try {
+                urlBuilder.append("?" + URLEncoder.encode("latlng","UTF-8") + "="
+                        +URLEncoder.encode(Double.toString(page.getUserPlace().getUserPlace().get_placeY()), "UTF-8")+","
+                        +URLEncoder.encode(Double.toString(page.getUserPlace().getUserPlace().get_placeX()), "UTF-8") ); /*여기는 위도 경도 y,x순*/
+                urlBuilder.append("&" + URLEncoder.encode("language","UTF-8") + "=" + URLEncoder.encode("ko", "UTF-8")); /*리턴 정보 한국어로 리턴*/
+                urlBuilder.append("&" + URLEncoder.encode("key","UTF-8") + "=" + URLEncoder.encode("AIzaSyCjdZL_BjLqCcj0PBKGcUP6kteb5tV2syE", "UTF-8")); /*키 값*/
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
 
+            URL url = null;
+            try {
+                url = new URL(urlBuilder.toString());
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+            assert url != null;
+            parsingUrl=url.toString();
+            //System.out.println(parsingUrl);
+
+            DocumentBuilderFactory dbFactory=DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder= null;
+            try {
+                dBuilder = dbFactory.newDocumentBuilder();
+            } catch (ParserConfigurationException e) {
+                e.printStackTrace();
+            }
+            Document doc= null;
+            try {
+                assert dBuilder != null;
+                doc = dBuilder.parse(parsingUrl);
+            } catch (IOException | SAXException e) {
+                e.printStackTrace();
+            }
+
+            doc.getDocumentElement().normalize();
+            //System.out.println("Root element : "+doc.getDocumentElement().getNodeName());
+
+            NodeList geoList=doc.getElementsByTagName("GeocodeResponse"); //장소 전체 노드
+
+            NodeList status=null;//doc.getElementsByTagName("status"); // 지역 노드
+            NodeList resultList=null;//doc.getElementsByTagName("result"); // 지역 노드
+
+            NodeList ComponentList=null;//doc.getElementsByTagName("address_component"); // 지역 노드
+
+            for(int i=0; i<geoList.getLength(); i++) {
+                Node geoNode=geoList.item(i);
+                if(geoNode.getNodeType()==Node.ELEMENT_NODE) {
+                    Element geoElement=(Element) geoNode;
+                    state= getTagValue("status",geoElement); // 상태(오류)
+                    resultList = geoElement.getElementsByTagName("result");
+                    Node resultNode=resultList.item(i);
+                    if(resultNode.getNodeType()==Node.ELEMENT_NODE){
+                        Element resultElement=(Element) resultNode;
+                        ComponentList =resultElement.getElementsByTagName("address_component");
+                        for(int c =0; c<ComponentList.getLength();c++){
+                            Node ComponentNode=ComponentList.item(c);
+                            if(ComponentNode.getNodeType()==Node.ELEMENT_NODE){
+                                Element ComponentElement=(Element) ComponentNode;
+                                if(getTagValue("type",ComponentElement)=="administrative_area_level_1"){
+                                    shortLocName=getTagValue("short_name",ComponentElement);
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            page.getUserPlace().getUserPlace().set_placeAddress(shortLocName); // 사용자가 있는 지역 이름 받음
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
+        }
+    }
 }
