05508959949a26daa6c70552f2141ea292995331
jimin755 <57628980+jimin755@users.noreply.github.com>
2020-11-23 20:34:02 +0900

    Update BusLane.java

--- app/src/main/java/com/example/project/BusLane.java
+++ app/src/main/java/com/example/project/BusLane.java
@@ -5,13 +5,11 @@ import java.util.ArrayList;
  * 1-9-3-5 교통 수단 정보 확장 노드 버스용 1..n
  */
 public class BusLane {
-    BusLane(String busNo, int type, int busID, int index){
+    BusLane(String busNo, int type, int busID){
         this.busID=busID;
         this.busNo=busNo;
         this.type=type;
-        this.index=index;
     }
-    private int index;
     private String busNo; //버스 번호 (버스인 경우에만 필수)
     private int type; //버스 코드 (버스인 경우에만 필수)
     private int busID; //버스 코드 (버스인 경우에만 필수)
@@ -25,14 +23,7 @@ public class BusLane {
     public String getBusNo() {
         return busNo;
     }
-
-    public int getIndex() {
-        return index;
-    }
-
-    public void setIndex(int index) {
-        this.index = index;
-    }
+    
     public void setBusID(int busID) {
         this.busID = busID;
     }
