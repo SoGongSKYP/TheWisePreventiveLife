8eaaa19e31be9fa7a1d0af0e6226a2c1d9ef9983
jimin755 <57628980+jimin755@users.noreply.github.com>
2020-11-23 21:38:01 +0900

    Update Lane.java

--- app/src/main/java/com/example/project/Lane.java
+++ app/src/main/java/com/example/project/Lane.java
@@ -2,29 +2,31 @@ package com.example.project;
 
 public class Lane {
 
-    Lane(String name, int subwayCodeORBusNo, int subwayCityCodeORBusId){
+    Lane(String name, int subwayCodeORtype, int subwayCityCodeORBusId){
         this.name =name;
-        this.subwayCodeORBusNo = subwayCodeORBusNo;
+        this.subwayCodeORtype = subwayCodeORtype;
         this.subwayCityCodeORBusId =subwayCityCodeORBusId;
 
     }
 
     private String name; //지하철 노선명 (지하철인 경우에만 필수)
-    private int subwayCodeORBusNo; //지하철 노선 번호 (지하철인 경우에만 필수)
+    private int subwayCodeORtype; //지하철 노선 번호 (지하철인 경우에만 필수)
     private int subwayCityCodeORBusId; //지하철 도시코드 (지하철인 경우에만 필수)
 
     public int getSubwayCityCodeORBusId() {
         return subwayCityCodeORBusId;
     }
-    public int getSubwayCodeORBusNo() {
-        return subwayCodeORBusNo;
+
+    public int getSubwayCodeORtype() {
+        return subwayCodeORtype;
+    }
+
+    public void setSubwayCodeORtype(int subwayCodeORtype) {
+        this.subwayCodeORtype = subwayCodeORtype;
     }
 
     public void setSubwayCityCodeORBusId(int subwayCityCodeORBusId) {
         this.subwayCityCodeORBusId = subwayCityCodeORBusId;
-    }
-    public void setSubwayCodeORBusNo(int subwayCodeORBusNo) {
-        this.subwayCodeORBusNo = subwayCodeORBusNo;
     }
 
     public String getName() {
