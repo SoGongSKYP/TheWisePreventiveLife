b88c3e1948abbc246f41c96113612c0756bffad1
jimin755 <57628980+jimin755@users.noreply.github.com>
2020-11-23 21:08:42 +0900

    Update Lane.java

--- app/src/main/java/com/example/project/Lane.java
+++ app/src/main/java/com/example/project/Lane.java
@@ -1,4 +1,36 @@
 package com.example.project;
 
 public class Lane {
+
+    Lane(String name, int subwayCodeORBusNo, int subwayCityCodeORBusId){
+        this.name =name;
+        this.subwayCodeORBusNo = subwayCodeORBusNo;
+        this.subwayCityCodeORBusId =subwayCityCodeORBusId;
+
+    }
+
+    private String name; //지하철 노선명 (지하철인 경우에만 필수)
+    private int subwayCodeORBusNo; //지하철 노선 번호 (지하철인 경우에만 필수)
+    private int subwayCityCodeORBusId; //지하철 도시코드 (지하철인 경우에만 필수)
+
+    public int getSubwayCityCodeORBusId() {
+        return subwayCityCodeORBusId;
+    }
+    public int getSubwayCodeORBusNo() {
+        return subwayCodeORBusNo;
+    }
+
+    public void setSubwayCityCodeORBusId(int subwayCityCodeORBusId) {
+        this.subwayCityCodeORBusId = subwayCityCodeORBusId;
+    }
+    public void setSubwayCodeORBusNo(int subwayCodeORBusNo) {
+        this.subwayCodeORBusNo = subwayCodeORBusNo;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
 }
