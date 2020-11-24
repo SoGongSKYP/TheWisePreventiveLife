cb75f515088afdf7bc35023bdb798b3c9240f110
jimin755 <57628980+jimin755@users.noreply.github.com>
2020-11-24 18:16:57 +0900

    Update FindPlace.java

--- app/src/main/java/com/example/project/FindPlace.java
+++ app/src/main/java/com/example/project/FindPlace.java
@@ -7,6 +7,8 @@ import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLEncoder;
 import java.util.ArrayList;
@@ -20,57 +22,77 @@ public class FindPlace {
     private ArrayList<Place> searchLocList;
 
     public FindPlace(String placeName) {
-        this.placeName=placeName;
-        this.searchLocList =new ArrayList<Place>();
+        this.placeName = placeName;
+        this.searchLocList = new ArrayList<Place>();
     }
+
     private static String getTagValue(String tag, Element eElement) {
-        NodeList nlList=eElement.getElementsByTagName(tag).item(0).getChildNodes();
-        Node nValue=(Node)nlList.item(0);
-        if(nValue==null) return null;
+        NodeList nlList = eElement.getElementsByTagName(tag).item(0).getChildNodes();
+        Node nValue = (Node) nlList.item(0);
+        if (nValue == null) return null;
         return nValue.getNodeValue();
     }
 
-    public ArrayList<Place> searchPlace() throws IOException, ParserConfigurationException, SAXException {
-        String parsingUrl="";
-        String key= "340FCCC5-C1C9-31D4-B7D8-56BC7558298A";
-        Place searchLoc=new Place("",0.0,0.0);
+    public void searchPlace() {
+        String parsingUrl = "";
+        String key = "340FCCC5-C1C9-31D4-B7D8-56BC7558298A";
+        Place searchLoc = new Place("", 0.0, 0.0);
         StringBuilder urlBuilder = new StringBuilder("http://api.vworld.kr/req/search?"); /*URL*/
-        urlBuilder.append(URLEncoder.encode("service","UTF-8") + "="+URLEncoder.encode("search", "UTF-8"));
-        urlBuilder.append("&" + URLEncoder.encode("request","UTF-8") + "="+ URLEncoder.encode("search", "UTF-8"));
-        urlBuilder.append("&" + URLEncoder.encode("version","UTF-8") + "=" + URLEncoder.encode("2.0", "UTF-8"));
-        urlBuilder.append("&" + URLEncoder.encode("query","UTF-8") + "=" + URLEncoder.encode(this.placeName, "UTF-8"));
-        urlBuilder.append("&" + URLEncoder.encode("type","UTF-8") + "=" + URLEncoder.encode("place", "UTF-8"));
-        urlBuilder.append("&" + URLEncoder.encode("format","UTF-8") + "=" + URLEncoder.encode("xml", "UTF-8"));
-        urlBuilder.append("&" + URLEncoder.encode("errorformat","UTF-8") + "=" + URLEncoder.encode("xml", "UTF-8"));
-        urlBuilder.append("&" + URLEncoder.encode("key","UTF-8") + "=" + URLEncoder.encode(key, "UTF-8"));
+        try {
+            urlBuilder.append(URLEncoder.encode("service", "UTF-8") + "=" + URLEncoder.encode("search", "UTF-8"));
+            urlBuilder.append("&" + URLEncoder.encode("request", "UTF-8") + "=" + URLEncoder.encode("search", "UTF-8"));
+            urlBuilder.append("&" + URLEncoder.encode("version", "UTF-8") + "=" + URLEncoder.encode("2.0", "UTF-8"));
+            urlBuilder.append("&" + URLEncoder.encode("query", "UTF-8") + "=" + URLEncoder.encode(placeName, "UTF-8"));
+            urlBuilder.append("&" + URLEncoder.encode("type", "UTF-8") + "=" + URLEncoder.encode("place", "UTF-8"));
+            urlBuilder.append("&" + URLEncoder.encode("format", "UTF-8") + "=" + URLEncoder.encode("xml", "UTF-8"));
+            urlBuilder.append("&" + URLEncoder.encode("errorformat", "UTF-8") + "=" + URLEncoder.encode("xml", "UTF-8"));
+            urlBuilder.append("&" + URLEncoder.encode("key", "UTF-8") + "=" + URLEncoder.encode(key, "UTF-8"));
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
 
-        URL url = new URL(urlBuilder.toString());
-        parsingUrl=url.toString();
-        //System.out.println(parsingUrl);
 
-        DocumentBuilderFactory dbFactory=DocumentBuilderFactory.newInstance();
-        DocumentBuilder dBuilder=dbFactory.newDocumentBuilder();
-        Document doc=dBuilder.parse(parsingUrl);
+        URL url = null;
+        try {
+            url = new URL(urlBuilder.toString());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        parsingUrl = url.toString();
+
+        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder dBuilder = null;
+        try {
+            dBuilder = dbFactory.newDocumentBuilder();
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        }
+        Document doc = null;
+        try {
+            doc = dBuilder.parse(parsingUrl);
+        } catch (IOException | SAXException e) {
+            e.printStackTrace();
+        }
         doc.getDocumentElement().normalize();
 
-        NodeList nList=doc.getElementsByTagName("item"); //장소 전체 노드
+        NodeList nList = doc.getElementsByTagName("item"); //장소 전체 노드
 
-        for(int i=0; i<nList.getLength(); i++) {
-            Node nNode=nList.item(i);
-            if(nNode.getNodeType()==Node.ELEMENT_NODE) {
-                Element eElement=(Element) nNode;
-                searchLoc.set_placeAddress(this.placeName);
-                searchLoc.set_placeDetailAddress(getTagValue("road",eElement));
-                searchLoc.set_placeX(Double.parseDouble(getTagValue("y",eElement)));
-                searchLoc.set_placeY(Double.parseDouble(getTagValue("x",eElement)));
-                this.searchLocList.add(searchLoc);
+        for (int i = 0; i < nList.getLength(); i++) {
+            Node nNode = nList.item(i);
+            if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+                Element eElement = (Element) nNode;
+                searchLoc.set_placeAddress(getTagValue("title", eElement));
+                searchLoc.set_placeDetailAddress(getTagValue("road", eElement));
+                searchLoc.set_placeX(Double.parseDouble(getTagValue("y", eElement)));
+                searchLoc.set_placeY(Double.parseDouble(getTagValue("x", eElement)));
+                searchLocList.add(searchLoc);
             }
         }
-        return this.searchLocList;
     }
+
+
 
     public ArrayList<Place> getSearchLocList() {
         return this.searchLocList;
     }
-
 }
