eaca46cc1a36b07f752fcfaaf8dc66d2d4e01d10
Psystar99 <40908279+Psystar99@users.noreply.github.com>
2020-11-26 04:54:52 +0900

    divide DB function

--- app/src/main/java/com/example/project/DBEntity.java
+++ app/src/main/java/com/example/project/DBEntity.java
@@ -1,5 +1,7 @@
 package com.example.project;
 
+import android.util.Log;
+
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -7,9 +9,12 @@ import java.util.Date;
 
 public class DBEntity{
     private static ArrayList<Patient> patientList;
-   // private DB db;
 
+    private DB task;
+    private String result="success";//일단 임시 success
+    private String sendmsg;
 
+    public DBEntity(){ }
     public void setPatientList(ArrayList<Patient> patientList){
         this.patientList = patientList;
     }
@@ -17,31 +22,40 @@ public class DBEntity{
         return patientList;
     }
 
-    public DBEntity() throws ParseException {
-        //1. 더미 데이트 객체 생성
-        String date1 = "2020-10-21";
-        String date2 = "2020-12-12";
-        //2. 더미 장소 객체 생성
-        Place dumP1=new Place("파주",1.1,1.2);
-        Place dumP2=new Place("서울",2.1,2.2);
+    /*인트로 때, 딱 한 번 불리는 함*/
+    public ArrayList<Patient>  patient_info(){
+        task= new DB("patients_info");
+        return patientList;
+    }
 
-        //3. 1,2로 확진자 방문장소 더미객체 생성
-        VisitPlace dumVP1=new VisitPlace(dumP1,date1);
-        VisitPlace dumVP2=new VisitPlace(dumP2,date1);
-        VisitPlace dumVP3=new VisitPlace(dumP1,date2);
-        //4. 3으로 확진자 방문장소 리스트 더미객체 생
-        ArrayList<VisitPlace> dumVSP1=new ArrayList<VisitPlace>();
-        ArrayList<VisitPlace> dumVSP2=new ArrayList<VisitPlace>();
-        dumVSP1.add(dumVP1);
-        dumVSP1.add(dumVP2);
-        dumVSP2.add(dumVP2);
-        dumVSP2.add(dumVP3);
-        //5. 확진자 두명
-        Patient dumPatient=new Patient(0,12,"1",date2,dumVSP1);
-        Patient dumPatient2=new Patient(0,1,"2",date1,dumVSP2);
-        patientList.add(dumPatient);
-        patientList.add(dumPatient2);
+    public int check_patient(String pnum, String plocnum){
+        if(result.equals("success")) return 1;
+        else return 0;
+    }
 
+    public int insert_patient(Patient patient){
+        if(result.equals("success")) return 1;
+        else return 0;
+    }
 
+    public int delete_patient(Patient patient){
+        if(result.equals("success")) return 1;
+        else return 0;
+    }
+
+    public int login(String managerID, String managerPW){
+        try {
+            sendmsg = "login";
+            task = new DB(sendmsg);
+            result = task.execute(managerID, managerPW, sendmsg).get();
+            Log.i("Servertest", "서버에서 받은 값" + result);
+
+            if (result.equals("success")) return 1;
+            else if (result.equals("failed")) return 0;
+            else if (result.equals("noId")) return 2;
+            else return -1;
+        }catch (Exception e) {
+            Log.i("DBtest", ".....ERROR.....!");
+            return -2;}
     }
 }
