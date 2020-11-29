0c3b33ad40bf7b207b1e26cc41eec195b3dc740c
Lee-sun-ah <62545246+Lee-sun-ah@users.noreply.github.com>
2020-11-29 14:03:43 +0900

    Update DBEntity.java

--- app/src/main/java/com/example/project/DBEntity.java
+++ app/src/main/java/com/example/project/DBEntity.java
@@ -6,6 +6,7 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.lang.reflect.Array;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -15,9 +16,11 @@ public class DBEntity {
 
     /*DBEntity에서 계속 저장할 환자 리스트*/
     private static ArrayList<Patient> patientList;
+    private static ArrayList<VisitPlace> dumVSP1;
+    private static ArrayList<VisitPlace> dumVSP2;
 
     /*DB 연동시 필요한 필드들*/
-    private DB task;
+    private static DB task;
     private String result = "success";//일단 임시 success
     private String sendmsg;
 
@@ -54,12 +57,15 @@ public class DBEntity {
     }
 
     /*관리자의 확진자 추가 페이지에서 확진자 동선 정보 하나를 추가하는 메소드-무조건 해당 환자에 한 정보가 테이블에 저장되어 있어야 함. */
-    public int AND_insert_pmoving(Patient patient, VisitPlace visitplace) {
+    public static int AND_insert_pmoving(Patient patient, VisitPlace visitplace) {
         for (int i = 0; i < patientList.size(); i++) {
+            Log.d("AND 환자 : ", patient.getPatientNum()+", "+Integer.toString(patient.getSmallLocalNum())+", "+Integer.toString(patient.getBigLocalNum()));
             if (patient.getSmallLocalNum() == patientList.get(i).getSmallLocalNum() || patient.getBigLocalNum() == patientList.get(i).getBigLocalNum()
                     || patient.getPatientNum() == patientList.get(i).getPatientNum()) {
                 //조건문 바꿔야 함! patient 테이블의 프라이머리 키는 지역번호랑 환자번호! 즉 patient와 patientList.get(i)의 정보가 같을 두객체가 같을 때——————————————————————————————————————————————————patient는 smalllocalnum,bigloculnum,patientnum 같으면 같은 객체!
+                Log.d("patient",Integer.toString(patientList.get(i).getVisitPlaceList().size()));
                 patientList.get(i).getVisitPlaceList().add(visitplace);
+                Log.d("patient",Integer.toString(patientList.get(i).getVisitPlaceList().size()));
                 //해당 환자에 visitplace 추가
                 return 1;// 동선 삽입 성공
             }
@@ -69,7 +75,8 @@ public class DBEntity {
 
 
     /*관리자의 확진자 수정 페이지에서 확진자 동선 정보 하나를 삭제하는 메소드 */
-    public int AND_delete_pmoving(Patient patient, VisitPlace visitplace) {
+    public static int AND_delete_pmoving(Patient patient, VisitPlace visitplace) {
+        //patient_info();
         for (int i = 0; i < patientList.size(); i++) {
             //1. 환자리스트를 돌며 동일한 환자 찾기.
             //DB테이블에서의 primary 키값들의 값이 같을 때 동일 환자.
@@ -78,6 +85,8 @@ public class DBEntity {
                     || patient.getPatientNum() == patientList.get(i).getPatientNum()){
                 //칮으면 해당 환자의 visitplacelist를 돌며 매개변수로 온 visitplace 찾고 삭제하기
                 patientList.get(i).getVisitPlaceList().remove(visitplace);
+                //setPatientList(patientList);
+                Log.d("patient",Integer.toString(patientList.get(i).getVisitPlaceList().size()));
                 return 1;//정상 삭제
             }
             return 0;//환자 정보는 있는데 visitplace 정보가 존재 x
@@ -95,9 +104,12 @@ public class DBEntity {
     //DB 테이블 수정----------------------------------------------------------------------------------------------------
 
     /*인트로 때, 앱 실행시 한 번만 불리는 메소드드*/
-    public ArrayList<Patient> patient_info() throws JSONException {
+    public static ArrayList<Patient> patient_info() throws JSONException {
+        patientList = new ArrayList<Patient>();
         task = new DB("patients_info");
-        ArrayList<VisitPlace> temp = new  ArrayList<VisitPlace>();
+        ArrayList<VisitPlace> temp = new ArrayList<VisitPlace>();
+        // int small=Integer.parseInt(plocnum.substring(0,1));
+        // int big=Integer.parseInt(plocnum.substring(2,3));
         //박소영 놈이 제대로 DB연동하기 전까지 만든 임시 확진자 리스트--> 구축 후 삭제 예정
 
         JSONArray patients = null;
@@ -118,7 +130,7 @@ public class DBEntity {
                             moving.getDouble("pointx"),moving.getDouble("pointy")),moving.getString("visitdate")));
                 }
             }
-            this.patientList.add(new Patient(small,big,patient.getString("pnum")
+            patientList.add(new Patient(small,big,patient.getString("pnum")
                     ,patient.getString("confirmdate"),temp));
         }
         return patientList;
